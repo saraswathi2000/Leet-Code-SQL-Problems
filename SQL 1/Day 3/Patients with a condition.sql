02288cf5ef26cda7765c8f8729980c2da30de531
Saraswathi <72403496+saraswathi2000@users.noreply.github.com>
2022-12-10 13:14:45 +0530

    Patients with conditions

---
+++ SQL 1/Day 3/Patients with a condition.sql
@@ -0,0 +1,23 @@
+/*
+TABLE : Patients
++--------------+---------+
+| Column Name  | Type    |
++--------------+---------+
+| patient_id   | int     |
+| patient_name | varchar |
+| conditions   | varchar |
++--------------+---------+
+patient_id is the primary key for this table.
+'conditions' contains 0 or more code separated by spaces. 
+This table contains information of the patients in the hospital.
+ 
+
+Write an SQL query to report the patient_id, patient_name and conditions of the patients who have Type I Diabetes. Type I Diabetes always starts with DIAB1 prefix.
+
+Return the result table in any order.*/
+
+
+SELECT patient_id,patient_name,conditions 
+from Patients 
+where conditions like '% DIAB1%' OR
+CONDITIONS LIKE 'DIAB1%';
