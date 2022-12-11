e9617c4c6b80264c52b2a664b19922783e7789b0
Saraswathi Pandit <72403496+saraswathi2000@users.noreply.github.com>
2022-12-11 14:24:11 +0530

    Second Highest Salary

---
+++ SQL 1/Day 4/Second Highest Salary.sql
@@ -0,0 +1,22 @@
+/*
+Table: Employee
+
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| id          | int  |
+| salary      | int  |
++-------------+------+
+id is the primary key column for this table.
+Each row of this table contains information about the salary of an employee.
+ 
+
+Write an SQL query to report the second highest salary from the Employee table. If there is no second highest salary, the query should report null.*/
+
+SELECT
+    IFNULL(
+      (SELECT DISTINCT Salary
+       FROM Employee
+       ORDER BY Salary DESC
+        LIMIT 1 OFFSET 1),
+    NULL) AS SecondHighestSalary
