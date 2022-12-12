7f1a8aff77372950984325b62a2d396814ed626b
Saraswathi Pandit <72403496+saraswathi2000@users.noreply.github.com>
2022-12-12 13:29:52 +0530

    Combine Two tables

--- SQL 1/DAY 5/Combine Two Tables.sql
+++ SQL 1/DAY 5/Combine Two Tables.sql
@@ -27,7 +27,7 @@ Each row of this table contains information about the city and state of one pers
 
 Write an SQL query to report the first name, last name, city, and state of each person in the Person table. If the address of a personId is not present in the Address table, report null instead.
 
-Return the result table in any order.*/
+Return the result table in any order./*
 
 
 SELECT p.firstName,p.lastName,a.city,a.state FROM Person p
