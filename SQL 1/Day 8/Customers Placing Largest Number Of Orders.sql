e4dc8a0bf9ee747ac74e41d6e50c534ade72ef11
Saraswathi Pandit <72403496+saraswathi2000@users.noreply.github.com>
2022-12-15 13:56:03 +0530

    customers placing large number of orders

---
+++ SQL 1/Day 8/Customers Placing Largest Number Of Orders.sql
@@ -0,0 +1,18 @@
+/* 
+Table: Orders
+
++-----------------+----------+
+| Column Name     | Type     |
++-----------------+----------+
+| order_number    | int      |
+| customer_number | int      |
++-----------------+----------+
+order_number is the primary key for this table.
+This table contains information about the order ID and the customer ID.
+ 
+
+Write an SQL query to find the customer_number for the customer who has placed the largest number of orders.*/
+
+select customer_number from orders
+group by customer_number
+order by count(customer_number) desc limit 1;
