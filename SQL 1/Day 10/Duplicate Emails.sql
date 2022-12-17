4ee3a6c8c1a092453b21e760ee2adfb4f2470dcb
Saraswathi Pandit <72403496+saraswathi2000@users.noreply.github.com>
2022-12-17 13:29:07 +0530

    Duplicate Emails

---
+++ SQL 1/Day 10/Duplicate Emails.sql
@@ -0,0 +1,21 @@
+
+/*Table: Person
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| email       | varchar |
++-------------+---------+
+id is the primary key column for this table.
+Each row of this table contains an email. The emails will not contain uppercase letters.
+ 
+
+Write an SQL query to report all the duplicate emails.
+
+Return the result table in any order.*/
+
+select Email
+from Person
+group by Email
+having count(*) > 1
