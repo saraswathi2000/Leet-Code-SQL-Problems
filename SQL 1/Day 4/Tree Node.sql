7e5565a5ab960cee7bae7043c424f5654e78eb94
Saraswathi Pandit <72403496+saraswathi2000@users.noreply.github.com>
2022-12-17 12:05:07 +0530

    Update tree node

--- SQL 1/Day 4/Tree Node.sql
+++ SQL 1/Day 4/Tree Node.sql
@@ -21,3 +21,10 @@ Write an SQL query to report the type of each node in the tree.
 
 Return the result table in any order.*/
 
+SELECT 
+    id,
+    CASE WHEN p_id IS NULL THEN 'Root'
+         WHEN id IN (SELECT p_id FROM tree) THEN 'Inner'
+         ELSE 'Leaf'
+    END AS Type
+FROM tree;
