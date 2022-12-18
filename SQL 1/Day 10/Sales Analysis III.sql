5b0640518bd64982fecfd559c7ba43222b990cb3
Saraswathi Pandit <72403496+saraswathi2000@users.noreply.github.com>
2022-12-18 21:23:46 +0530

    sales Analysis

---
+++ SQL 1/Day 10/Sales Analysis III.sql
@@ -0,0 +1,43 @@
+/*
+Table: Product
+
++--------------+---------+
+| Column Name  | Type    |
++--------------+---------+
+| product_id   | int     |
+| product_name | varchar |
+| unit_price   | int     |
++--------------+---------+
+product_id is the primary key of this table.
+Each row of this table indicates the name and the price of each product.
+Table: Sales
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| seller_id   | int     |
+| product_id  | int     |
+| buyer_id    | int     |
+| sale_date   | date    |
+| quantity    | int     |
+| price       | int     |
++-------------+---------+
+This table has no primary key, it can have repeated rows.
+product_id is a foreign key to the Product table.
+Each row of this table contains some information about one sale.
+ 
+
+Write an SQL query that reports the products that were only sold in the first quarter of 2019. That is, between 2019-01-01 and 2019-03-31 inclusive.
+
+Return the result table in any order.*/
+
+SELECT product_id, product_name
+FROM product
+WHERE product_id NOT IN (
+SELECT p.product_id
+FROM product AS p
+INNER JOIN sales AS s
+ON p.product_id = s.product_id
+WHERE s.sale_date > '2019-03-31'
+OR s.sale_date < '2019-01-01'
+);
