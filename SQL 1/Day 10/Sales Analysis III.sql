dfcddaf15bf5f0fb9f96efb45061c8b9102ecd59
Saraswathi Pandit <72403496+saraswathi2000@users.noreply.github.com>
2022-12-19 11:38:49 +0530

    Update Sales Analysis III.sql

--- SQL 1/Day 10/Sales Analysis III.sql
+++ SQL 1/Day 10/Sales Analysis III.sql
@@ -29,7 +29,7 @@ Each row of this table contains some information about one sale.
 
 Write an SQL query that reports the products that were only sold in the first quarter of 2019. That is, between 2019-01-01 and 2019-03-31 inclusive.
 
-Return the result table in any order.*/
+Return the result table in any order..*/
 
 SELECT product_id, product_name
 FROM product
