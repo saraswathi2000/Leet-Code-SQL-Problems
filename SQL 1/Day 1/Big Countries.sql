67de218da3c259d949981f6dee46d2588bdf8c08
Saraswathi Pandit <72403496+saraswathi2000@users.noreply.github.com>
2022-12-24 23:38:51 +0530

    Update Big Countries.sql

--- SQL 1/Day 1/Big Countries.sql
+++ SQL 1/Day 1/Big Countries.sql
@@ -15,7 +15,7 @@ Each row of this table gives information about the name of a country, the contin
 A country is big if:
 it has an area of at least three million (i.e., 3000000 km2), or
 it has a population of at least twenty-five million (i.e., 25000000).
-Write an SQL query to report the name, population, and area of the big countries.
+Write an SQL query to report the name of big countries, population, and area of the big countries.
 Return the result table in any order.
 */
 
